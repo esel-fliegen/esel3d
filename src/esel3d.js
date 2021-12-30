c5578104000d532ffee227f946dd62be91063009
esel-fliegen <esel.fliegen@gmail.com>
2021-12-30 10:28:27 -0500

    update grid

--- src/esel3d.js
+++ src/esel3d.js
@@ -498,8 +498,8 @@ class RectGridClass {
   }
 
   countDecimals(num){
-    if ((value % 1) != 0) 
-        return value.toString().split(".")[1].length;  
+    if ((num % 1) != 0) 
+        return num.toString().split(".")[1].length;  
     return 0;
   }
   xNum(){
